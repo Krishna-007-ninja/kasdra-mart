4f1ad32ea8edc8b661e01023c352d12d589f1ddd
Krishna Kumar <rautkrishno@gmail.com>
2026-05-18 12:28:17 +0530

    Updated API base URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://kasdra-mart.onrender.com/',
 });
 
 // Add a request interceptor to include the token in headers
