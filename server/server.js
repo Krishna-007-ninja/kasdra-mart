2e3f486413d2167270da1ab590db6f893a3d6552
Krishna Kumar <rautkrishno@gmail.com>
2026-05-18 13:28:22 +0530

    Fixed production API base URL

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use('/api', limiter);
 // CORS Configuration
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'https://kasdra-mart.vercel.app', // Vite default port
+    origin: process.env.CLIENT_URL || 'https://kasdra-mart.vercel.app/', // Vite default port
     credentials: true,
   })
 );
