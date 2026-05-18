cdb663fcfcd9d53d78be3325fba791833ab34105
Krishna Kumar <rautkrishno@gmail.com>
2026-05-18 13:21:16 +0530

    Fixed production API base URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,24 +1,25 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://kasdra-mart.onrender.com/',
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    'http://localhost:5000/api',
 });
 
-// Add a request interceptor to include the token in headers
+// Add token automatically
 api.interceptors.request.use(
   (config) => {
     const userInfo = localStorage.getItem('userInfo')
       ? JSON.parse(localStorage.getItem('userInfo'))
       : null;
 
-    if (userInfo && userInfo.token) {
+    if (userInfo?.token) {
       config.headers.Authorization = `Bearer ${userInfo.token}`;
     }
+
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
